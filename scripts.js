95102432b9dba0675f0ee2500e0fdce507f11861
MatchaT13 <brian13@my.yorku.ca>
2025-08-05 15:09:46 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const blogList = document.getElementById('blog-list');
+
+    fetch('posts.json')
+    .then(response => response.json())
+    .then(posts => {
+        posts.forEach(post => {
+            const postElement = document.createElement('div');
+            postElement.classList.add('post');
+
+            postElement.innerHTML = `
+            <h2>${post.title}</h2>
+            <p class="date">${post.date}</p>
+            <p>${post.content}</p>
+            `;
+
+            blogList.appendChild(postElement);
+        });
+    })
+    .catch(error => console.error('Error loading blog posts:', error))
+});
